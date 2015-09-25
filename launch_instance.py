f05b114ad4a8f52eccec59dcd569238e772a91e2
e-koch <koch.eric.w@gmail.com>
2015-09-24 19:54:33 -0600

    Actually launch instance

--- launch_instance.py
+++ launch_instance.py
@@ -3,8 +3,15 @@
 import boto
 
 
-def launch():
+def launch(key_name, region='us-west-2', image_id='ami-5189a661',
+           instance_type='t2.micro'):
     '''
     '''
 
-    ec2 = boto.connect_ec2()
+    ec2 = boto.ec2.connect_to_region(region)
+
+    ec2.run_instances(image_id, key_name=key_name, instance_type=instance_type)
+
+    return ec2
+
+    # ec2.get_instance_attribute('i-336b69f6', 'instanceType')
