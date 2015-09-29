e8c68ccc843e59c72ad8ebe80d50ad3a0d5afb35
e-koch <koch.eric.w@gmail.com>
2015-09-28 22:34:59 -0600

    security_groups

--- launch_instance.py
+++ launch_instance.py
@@ -6,18 +6,18 @@ import time
 
 
 def launch(key_name, region='us-west-2', image_id='ami-5189a661',
-           instance_type='t2.micro', group_names='launch-wizard-1'):
+           instance_type='t2.micro', security_groups='launch-wizard-1'):
     '''
     '''
 
-    if not isinstance(group_names, list):
-        group_names = [group_names]
+    if not isinstance(security_groups, list):
+        security_groups = [security_groups]
 
     ec2 = boto.ec2.connect_to_region(region)
 
     reserve = ec2.run_instances(image_id, key_name=key_name,
                                 instance_type=instance_type,
-                                group_names=group_names)
+                                security_groups=security_groups)
 
     inst = reserve.instances[0]
 
