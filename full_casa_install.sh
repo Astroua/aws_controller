1d28f1132428f7a5b5133f040c5084cc7cb8fdfc
e-koch <koch.eric.w@gmail.com>
2015-10-28 10:54:09 -0600

    Source profile so CASA appears in path

--- full_casa_install.sh
+++ full_casa_install.sh
@@ -6,11 +6,15 @@ sh $HOME/.profile
 casa_version=${1}
 
 sh $HOME/aws_controller/casa-deploy/deploy_casa${casa_version}.sh
+sh $HOME/.profile
 
 sh $HOME/aws_controller/casa-deploy/install_casa_pip.sh
+sh $HOME/.profile
 
 sh $HOME/aws_controller/casa-deploy/install_casa_packages.sh
+sh $HOME/.profile
 
 sh $HOME/aws_controller/casa-deploy/install_uvmultifit.sh $HOME/aws_controller/casa-deploy/external_packages/uvmultifit/
+sh $HOME/.profile
 
 sh $HOME/aws_controller/casa-deploy/install_casa_analysis_scripts.sh
