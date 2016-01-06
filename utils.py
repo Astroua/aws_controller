171ac252368bcda64d2849a68a7059429f83ec57
e-koch <koch.eric.w@gmail.com>
2016-01-06 12:41:30 -0700

    Human readable time output

--- utils.py
+++ utils.py
@@ -2,11 +2,17 @@
 
 from datetime import datetime
 import os
+import time
 
 
 def timestring():
     TimeString = datetime.now().strftime("%Y%m%d%H%M%S%f")
     return TimeString
+
+
+def human_time():
+    now = datetime.fromtimestamp(time.time())
+    return now.strftime('%Y-%m-%d %H:%M:%S')
 
 
 def listdir_fullpath(d):
